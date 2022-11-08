a3b118eabe9c9e6da95759ed020fecf8af0f674e
Shenghang Zhu <67240191+zyhzsh@users.noreply.github.com>
2022-11-08 12:04:47 +0100

    Layout-Components-SplitScreenExample

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { SplitScreen } from "./SplitScreen";
+
+const LeftHand = ()=>{
+  return <h1>Left</h1>
+}
+const RightHand = ()=>{
+  return <p>Right</p>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SplitScreen
+      // left={LeftHand}
+      // right={RightHand}
+      leftWeight={1}
+      rightWeight={3}
+    >
+      <LeftHand/>
+      <RightHand/>
+    </SplitScreen>
   );
 }
 
