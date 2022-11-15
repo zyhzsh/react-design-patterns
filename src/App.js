9ea403e6c4338cb98a87e0192bbcc66966fb7f79
Shenghang Zhu <67240191+zyhzsh@users.noreply.github.com>
2022-11-15 15:18:31 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import { printProps } from './printProps';
-import { UserInfo } from './UserInfo';
-import { withUser } from './withUser';
+
 import { UserInfoForm } from './UserInfoForm';
 
-const UserInfoWithLoader = withUser(UserInfo, '234');
 
 function App() {
 	return (
